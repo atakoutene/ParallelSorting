502b3de4c1a0f19385514ed246f99a376e6df1f4
Yann Djoumessi <yanndjoumessi@gmail.com>
2024-06-04 23:31:40 -0400

    Created the thread pool for efficient sorting

--- src/src/main/java/org/atakoutene/Main.java
+++ src/src/main/java/org/atakoutene/Main.java
@@ -1,8 +1,34 @@
 package org.atakoutene;
+import java.concurrent.*;
+
 
 public class Main {
     public static void main(String[] args) {
+        int SIZE = 10000;
+        ArrayList<Integer> array = new int[SIZE];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        ForkJoinPool pool = new ForkJoinPool();
+        MergeSortTask task = new MergeSortTask(array, 0, SIZE);
 
-        System.out.println("Hello world!");
+        Long startTime = System.currentTimeMillis();
+        ArrayList<Integer> sortedList = pool.invoke(task);
+        Long endTime = System.currentTimeMillis();
+
+        System.out.println("The sorted elements are: ");
+        sortedList.forEach((el) -> {
+            System.out.print(el + " ");
+        })
+
+        System.out.println("\nElapsed Time: " + endTime - startTime + "ms");
+    }
+
+    public static void fillarray(int[] array) {
+        int SIZE = array.length;
+        Random random = new Random();
+        for(int i = 0; i < SIZE; i++) {
+            array[i] = random.nextInt();
+        }
     }
 }
