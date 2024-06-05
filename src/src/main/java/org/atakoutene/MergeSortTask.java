5ab4e5aaad6a86044c5f20e04d138a664ced1a02
atakoutene <atakoutene@gmail.com>
2024-06-04 22:58:05 -0400

    create MergeSortTask.

---
+++ src/src/main/java/org/atakoutene/MergeSortTask.java
@@ -0,0 +1,11 @@
+package org.atakoutene;
+
+import java.util.concurrent.RecursiveTask;
+
+public class MergeSortTask extends RecursiveTask<Integer> {
+    private static final int THRESHOLD = 150;
+    @Override
+    protected Integer compute() {
+        return 0;
+    }
+}
